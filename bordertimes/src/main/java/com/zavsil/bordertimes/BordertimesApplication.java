631dbd053dddf377102de7d77e1e3017088dc3ff
francisco.zavala <francisco.zavala@uabc.edu.mx>
2021-11-02 13:29:42 -0700

    primer commit

---
+++ bordertimes/src/main/java/com/zavsil/bordertimes/BordertimesApplication.java
@@ -0,0 +1,13 @@
+package com.zavsil.bordertimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordertimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordertimesApplication.class, args);
+	}
+
+}
